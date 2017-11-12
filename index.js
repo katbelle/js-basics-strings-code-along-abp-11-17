5ebdabdac74c9586a38c296a05ceb6b893afdb41
Katherine Belle Hartling <katbellehartling@gmail.com>
2017-11-12 17:46:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+<<<<<<< HEAD
+=======
+
+>>>>>>> b4f84c61a8333e51f4e7de79413dcb3bd1154a44
